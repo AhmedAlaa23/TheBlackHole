c2ee5b32331e04860097bcf8cfc0093a90e161a1
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2015-07-06 01:46:19 +0200

    new equations and black bg

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function blackhole(ctx){
 	this.x=ctx.canvas.width/2;
 	this.y=ctx.canvas.height/2;
 	this.r= ctx.canvas.width/60;
-	this.mass=100000;
+	this.mass=1000;
 	this.vx=0;
 	this.vy=0;
 	
@@ -18,12 +18,12 @@ function blackhole(ctx){
 function circle(){
 	this.x=300;
 	this.y=300;
-	this.r= cw/120;
-	this.mass=1000;
+	this.r= cw/160;
+	this.mass=100;
 	this.vx=0;				// velocity in the x axis
 	this.vy=5;				// velocity in the y axis and the initial velocity is 5 in the Y axis
 	this.color = "blue";
-	this.massscore=100;
+	this.massscore=10;
 	
 	this.render = function(ctx){
 		ctx.beginPath();
@@ -52,10 +52,10 @@ function init(){
 	var trace = 1; 	// 1 for shawing the trace behind the object
 	var showscore = document.getElementById("scorenumber");
 	
-	var r= cw/120;		// initial values that change later when choosing moon or sun
-	var mass=1000;
+	var r= cw/160;		// initial values that change later when choosing moon or sun
+	var mass=100;
 	var color="blue";
-	var massscore=100;
+	var massscore=10;
 	
 	var hole = new blackhole(ctx);	// init the balckhole
 	
@@ -64,12 +64,14 @@ function init(){
 	circs[i].y = (ch/2.7);
 	i++;
 	
+
 	function animate(){
 		if(trace == -1){						// remove the trace
-			ctx.clearRect(0,0,cw,ch);
+			ctx.fillStyle= "rgba(18,18,18, 1)";
+			ctx.fillRect(0,0,cw,ch);
 		}
 		else{
-			ctx.fillStyle= "rgba(255,255,255, 0.1)";
+			ctx.fillStyle= "rgba(0,0,0, 0.1)";
 			ctx.fillRect(0,0,cw,ch);
 		}
 		
@@ -88,7 +90,6 @@ function init(){
 		
 		if(p != 1 && gover != 1){
 			window.requestAnimationFrame(animate);
-			//animrate =  setInterval(animate, 1000);
 		}
 	}
 	animate();
@@ -97,14 +98,14 @@ function init(){
 	function calcv(object){		// calculate ths velocity and detect gameover and calc the score
 		// the radius between the selected object and the others cilcles (a^2 = b^2 + c^2)
 		var hrad = Math.sqrt(Math.pow(Math.abs(hole.y-circs[object].y),2) + Math.pow(Math.abs(hole.x-circs[object].x),2));
-		
-		//circs[object].vy += ( ( ( (hole.y-circs[object].y) * hole.mass) /(hrad) ) + ((circs[object].mass*Math.pow(circs[object].vy,2))/hrad) )/1000000;
-		//circs[object].vx += ( ( ( (hole.x-circs[object].x) * hole.mass) /(hrad) ) + ((circs[object].mass*Math.pow(circs[object].vx,2))/hrad) )/1000000;
-		// 1000000
-		var vspeed = 1000000;
-		circs[object].vy += ( ( (hole.y-circs[object].y) * (hole.mass + circs[object].mass) ) / (hrad+circs[object].mass/100) )  /vspeed;	// add velocity
+
+		circs[object].vy += ( ((hole.y-circs[object].y) + (hole.mass/hrad) ) / (hrad*10) );  //vspeed;	// add velocity
 	
-		circs[object].vx += ( ( (hole.x-circs[object].x) * (hole.mass + circs[object].mass) ) / (hrad+circs[object].mass/100) ) /vspeed;
+		circs[object].vx += ( ((hole.x-circs[object].x) + (hole.mass/hrad) ) / (hrad*10) ); //vspeed;
+
+		// old equations
+		//circs[object].vy += ( ( (hole.y-circs[object].y) * (hole.mass + circs[object].mass) ) / (hrad+circs[object].mass/100) )  /vspeed;	// add velocity
+		//circs[object].vx += ( ( (hole.x-circs[object].x) * (hole.mass + circs[object].mass) ) / (hrad+circs[object].mass/100) ) /vspeed;
 		
 		
 		// check if an object hit the black hole
@@ -115,15 +116,18 @@ function init(){
 			document.getElementById("ng").style.display = "inline-block";
 		}
 		
-		if(circs.length>1){		// start adding score when the user add an object
+		// start adding score when the user add an object
+		if(circs.length>1){
 			score += (circs[object].massscore / hrad); // Adding the score
 		}
 		
-		for(k=0; k<circs.length; k++){	// calc the velocity for every object with the ohter objects
+		// calc the velocity for every object with the ohter objects
+		for(k=0; k<circs.length; k++){
 			if(object != k){
 				var orad = Math.sqrt(Math.pow(Math.abs(circs[k].y-circs[object].y),2) + Math.pow(Math.abs(circs[k].x-circs[object].x),2));
 				
-				if(orad <= circs[k].r){ 	// check if two objects hit each others
+				// check if two objects hit each others
+				if(orad <= circs[k].r){
 					gover=1;
 					document.getElementById("state").style.display = "inline-block";
 					document.getElementById("state").innerHTML = "Game Over <br>Two Objects has hit each other :(<br><span id='scorespan'>Your Score is: "+Math.floor(score)+"</span>";
@@ -131,21 +135,23 @@ function init(){
 				}
 				
 				// v,x velocity = gravitational force + centripetal force = ((y-y) * mass)/r^2 + (mass*velocity^2)/r^2
-				//circs[object].vy += ( ( ( (circs[k].y-circs[object].y) * circs[k].mass ) /orad ) + ((circs[object].mass*Math.pow(circs[object].vy,2))/orad) )/1000000;
-				//circs[object].vx += ( ( ( (circs[k].x-circs[object].x) * circs[k].mass ) /orad ) + ((circs[object].mass*Math.pow(circs[object].vx,2))/orad) )/1000000;
-			
-				circs[object].vy += ( ( (circs[k].y-circs[object].y) * (circs[k].mass + circs[object].mass ) ) / (orad+circs[object].mass/100) )  /1000000;
+				if(circs[k].mass >= circs[object].mass){
+					circs[object].vy += ( ((circs[k].y-circs[object].y)/2) + (circs[k].mass / orad) ) / (orad*10);  //vspeed;	// add velocity
 	
-				circs[object].vx += ( ( (circs[k].x-circs[object].x) * (circs[k].mass + circs[object].mass ) ) / (orad+circs[object].mass/100) ) /1000000;
-				
+					circs[object].vx += ( ((circs[k].x-circs[object].x)/2) + (circs[k].mass / orad) ) / (orad*10); //vspeed;
+				}
+
+				// old equations
+				//circs[object].vy += ( ( (circs[k].y-circs[object].y) * (circs[k].mass + circs[object].mass ) ) / (orad+circs[object].mass/100) )  /vspeed;
+				//circs[object].vx += ( ( (circs[k].x-circs[object].x) * (circs[k].mass + circs[object].mass ) ) / (orad+circs[object].mass/100) ) /vspeed;
 			}
 		}
 		
 	}
 	
 	
-	
-	ctx.canvas.addEventListener("click", function(event){		// when mouse clicked on the canvas it create a new object
+	// when mouse clicked on the canvas it create a new object
+	ctx.canvas.addEventListener("click", function(event){
 		var mousex = event.clientX - ctx.canvas.offsetLeft;	// to get the coordinates inside the canvas right if there is a space left in the page
 		var mousey = event.clientY - ctx.canvas.offsetTop;
 		
@@ -175,32 +181,32 @@ function init(){
 		}
 	});
 	
-	document.getElementById("sun").addEventListener("click", function(){
-		r= cw/80;		// setting the values of the sun that will be set to it when created
-		mass=10000;
+	document.getElementById("star").addEventListener("click", function(){
+		r= cw/100;		// setting the values of the sun that will be set to it when created
+		mass=500;
 		color="orange";
-		massscore=500;
-		document.getElementById("sun").style.color = "brown";
+		massscore=20;
+		document.getElementById("star").style.color = "brown";
 		document.getElementById("planet").style.color = "black";
 		document.getElementById("moon").style.color = "black";
 	});
 	
 	document.getElementById("planet").addEventListener("click", function(){
-		r= cw/120;
-		mass=1000;
+		r= cw/160;
+		mass=100;
 		color="blue";
-		massscore=100;
-		document.getElementById("sun").style.color = "black";
+		massscore=10;
+		document.getElementById("star").style.color = "black";
 		document.getElementById("planet").style.color = "brown";
 		document.getElementById("moon").style.color = "black";
 	});
 	
 	document.getElementById("moon").addEventListener("click", function(){
-		r= cw/200;
-		mass=100;
+		r= cw/250;
+		mass=1;
 		color="gray";
-		massscore=10;
-		document.getElementById("sun").style.color = "black";
+		massscore=1;
+		document.getElementById("star").style.color = "black";
 		document.getElementById("planet").style.color = "black";
 		document.getElementById("moon").style.color = "brown";
 	});
@@ -210,6 +216,7 @@ function init(){
 		
 		if(trace==1){
 			document.getElementById("trace").innerHTML = "Hide Trace";
+			ctx.clearRect(0,0,cw,ch);
 		}
 		else if(trace==-1){
 			document.getElementById("trace").innerHTML = "Show Trace";
